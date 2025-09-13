3c89124d658c361daa5d382c72ee9b3e18c05e8c
disco252 <printsandturns@gmail.com>
2025-09-13 07:47:24 -0500

    Update SATpwn.py

--- SATpwn.py
+++ SATpwn.py
@@ -157,8 +157,12 @@ class SATpwn(plugins.Plugin):
         logging.info("[SATpwn] plugin ready")
     
     def on_ui_setup(self, ui):
-        ui.add_element('sat_mode', components.Text(color=view.WHITE, value=f'SAT Mode: {self.mode.capitalize()}',
-                                                  position=(5, 5)))
+        logging.info("[SATpwn] on_ui_setup() called")
+        ui.add_element('sat_mode', components.Text(
+        color=view.WHITE,
+        value=f'SAT Mode: {self.mode.capitalize()}',
+        position=(5,5)))
+        logging.info("[SATpwn] UI element added")
     
     def on_ui_update(self, ui):
         ui.set('sat_mode', f'SAT Mode: {self.mode.capitalize()}')
